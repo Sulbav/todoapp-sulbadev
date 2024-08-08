4f0098956b62457c684ddeafde6ad5c3dfb28300
Abraham Sulbaran <abrahamsulbaram@gmail.com>
2024-08-07 21:17:47 -0400

    empezando la lista de usuarios

--- main.js
+++ main.js
@@ -1,37 +1,27 @@
-const nombre = document.getElementById("name")
-const password = document.getElementById("password")
-const mail = document.getElementById("mail")
+import { dibujarUsuarios,nuevoUsuario } from "./ulogic"
+
+window.addEventListener("load",e=>{
+  dibujarUsuarios()
+})
+
+const modal = document.querySelector(".modal")
+const abrirModal = document.getElementById("abrir-modal")
+abrirModal.addEventListener("click",e=>{
+  modal.style.opacity = 1
+  modal.style.pointerEvents = "fill"
+})
+
 const form = document.getElementById("sesion")
-
-let listLocal = localStorage.getItem("usuarios");
-let usersList = listLocal==null?[]:JSON.parse(listLocal);
-
-const nuevoUsuario = (name,contra,mail)=>{
-  const newUser ={
-    name,
-    contra,
-    mail
-  }
-
-  usersList.push(newUser)
-  guardarLUsuarios(usersList)
-}
-
-const guardarLUsuarios = (lista)=>{
-  localStorage.setItem("usuarios",JSON.stringify(lista))
-}
-
 form.addEventListener("submit",e=>{
-e.preventDefault();
-  let user = nombre.value
-  let contra = password.value
-  let email = mail.value
+  let user = document.getElementById("nombre").value
+  let password = document.getElementById("password").value
 
-  if (nombre.lengt >= 4 && password.lengt>= 5) {
-    nuevoUsuario(nombre,password,email)
+  if (user.length >= 4 && password.length >= 5) {
+    nuevoUsuario(user,password)
   console.log(localStorage.usuarios)
   }else{
-    console.log("no registrado")
+    e.preventDefault()
+    alert("no registrado")
   }
 })
 
