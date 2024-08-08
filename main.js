71d674fb5fa04ec610147f8c2abe160ab8dbb579
Abraham Sulbaran <abrahamsulbaram@gmail.com>
2024-08-07 21:36:47 -0400

    commit-ignore

--- main.js
+++ main.js
@@ -1,28 +1,26 @@
-import { dibujarUsuarios,nuevoUsuario } from "./ulogic"
+import { dibujarUsuarios, nuevoUsuario } from "./ulogic";
 
-window.addEventListener("load",e=>{
-  dibujarUsuarios()
-})
+window.addEventListener("load", (e) => {
+  dibujarUsuarios();
+});
 
-const modal = document.querySelector(".modal")
-const abrirModal = document.getElementById("abrir-modal")
-abrirModal.addEventListener("click",e=>{
-  modal.style.opacity = 1
-  modal.style.pointerEvents = "fill"
-})
+const modal = document.querySelector(".modal");
+const abrirModal = document.getElementById("abrir-modal");
+abrirModal.addEventListener("click", (e) => {
+  modal.style.opacity = 1;
+  modal.style.pointerEvents = "fill";
+});
 
-const form = document.getElementById("sesion")
-form.addEventListener("submit",e=>{
-  let user = document.getElementById("nombre").value
-  let password = document.getElementById("password").value
+const form = document.getElementById("sesion");
+form.addEventListener("submit", (e) => {
+  let user = document.getElementById("nombre").value;
+  let password = document.getElementById("password").value;
 
   if (user.length >= 4 && password.length >= 5) {
-    nuevoUsuario(user,password)
-  console.log(localStorage.usuarios)
-  }else{
-    e.preventDefault()
-    alert("no registrado")
+    nuevoUsuario(user, password);
+    console.log(localStorage.usuarios);
+  } else {
+    e.preventDefault();
+    alert("no registrado");
   }
-})
-
-
+});
--- ulogic.js
+++ ulogic.js
@@ -1,27 +1,26 @@
 let listLocal = JSON.parse(localStorage.getItem("usuarios"));
-let usersList = listLocal==null?[]:listLocal;
+let usersList = listLocal == null ? [] : listLocal;
 
-export const dibujarUsuarios = ()=>{
-const lista = document.querySelector(".users-list")
-  usersList.map((user,index)=>{
+export const dibujarUsuarios = () => {
+  const lista = document.querySelector(".users-list");
+  usersList.map((user, index) => {
     lista.innerHTML += `<section class="user" index=${index}>
     <span>${user.name}</span>
     <span>${index}</span>
-    </section>`
-  })
-}
+    </section>`;
+  });
+};
 
+export const nuevoUsuario = (name, contra) => {
+  const newUser = {
+    name,
+    contra,
+  };
 
-export const nuevoUsuario = (name,contra)=>{
-    const newUser ={
-      name,
-      contra
-    }
-  
-    usersList.push(newUser)
-    guardarLUsuarios(usersList)
-  }
-  
-  const guardarLUsuarios = (lista)=>{
-    localStorage.setItem("usuarios",JSON.stringify(lista))
-  }
+  usersList.push(newUser);
+  guardarLUsuarios(usersList);
+};
+
+const guardarLUsuarios = (lista) => {
+  localStorage.setItem("usuarios", JSON.stringify(lista));
+};
